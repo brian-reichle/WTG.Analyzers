23956485699c0c82deb186127099f5eacc7b3af1
brian reichle <brian.reichle@wisetechglobal.com>
2019-01-22 20:49:20 +1100

    Merge LiteralSyntaxFactory into ExpressionSyntaxFactory.

--- WTG.Analyzers/Analyzers/Emit/ConversionVisitor.cs
+++ WTG.Analyzers/Analyzers/Emit/ConversionVisitor.cs
@@ -35,10 +35,14 @@ namespace WTG.Analyzers
 			{
 				switch (operandType)
 				{
-					case OpCodeOperand.InlineI: return LiteralSyntaxFactory.CreateInt(Convert.ToInt32(node.Token.Value, CultureInfo.InvariantCulture));
-					case OpCodeOperand.InlineI8: return LiteralSyntaxFactory.CreateLong(Convert.ToInt64(node.Token.Value, CultureInfo.InvariantCulture));
-					case OpCodeOperand.ShortInlineR: return LiteralSyntaxFactory.CreateFloat(Convert.ToSingle(node.Token.Value, CultureInfo.InvariantCulture));
-					case OpCodeOperand.InlineR: return LiteralSyntaxFactory.CreateDouble(Convert.ToDouble(node.Token.Value, CultureInfo.InvariantCulture));
+					case OpCodeOperand.InlineI:
+						return ExpressionSyntaxFactory.CreateLiteral(Convert.ToInt32(node.Token.Value, CultureInfo.InvariantCulture));
+					case OpCodeOperand.InlineI8:
+						return ExpressionSyntaxFactory.CreateLiteral(Convert.ToInt64(node.Token.Value, CultureInfo.InvariantCulture));
+					case OpCodeOperand.ShortInlineR:
+						return ExpressionSyntaxFactory.CreateLiteral(Convert.ToSingle(node.Token.Value, CultureInfo.InvariantCulture));
+					case OpCodeOperand.InlineR:
+						return ExpressionSyntaxFactory.CreateLiteral(Convert.ToDouble(node.Token.Value, CultureInfo.InvariantCulture));
 				}
 			}
 
@@ -51,16 +55,23 @@ namespace WTG.Analyzers
 		{
 			switch (operandType)
 			{
-				case OpCodeOperand.ShortInlineI: return SyntaxFactory.PredefinedType(SyntaxFactory.Token(SyntaxKind.SByteKeyword));
-				case OpCodeOperand.ShortInlineVar: return SyntaxFactory.PredefinedType(SyntaxFactory.Token(SyntaxKind.ByteKeyword));
+				case OpCodeOperand.ShortInlineI:
+					return SyntaxFactory.PredefinedType(SyntaxFactory.Token(SyntaxKind.SByteKeyword));
+				case OpCodeOperand.ShortInlineVar:
+					return SyntaxFactory.PredefinedType(SyntaxFactory.Token(SyntaxKind.ByteKeyword));
 
-				case OpCodeOperand.InlineI: return SyntaxFactory.PredefinedType(SyntaxFactory.Token(SyntaxKind.IntKeyword));
-				case OpCodeOperand.InlineVar: return SyntaxFactory.PredefinedType(SyntaxFactory.Token(SyntaxKind.ShortKeyword));
+				case OpCodeOperand.InlineI:
+					return SyntaxFactory.PredefinedType(SyntaxFactory.Token(SyntaxKind.IntKeyword));
+				case OpCodeOperand.InlineVar:
+					return SyntaxFactory.PredefinedType(SyntaxFactory.Token(SyntaxKind.ShortKeyword));
 
-				case OpCodeOperand.InlineI8: return SyntaxFactory.PredefinedType(SyntaxFactory.Token(SyntaxKind.LongKeyword));
+				case OpCodeOperand.InlineI8:
+					return SyntaxFactory.PredefinedType(SyntaxFactory.Token(SyntaxKind.LongKeyword));
 
-				case OpCodeOperand.InlineR: return SyntaxFactory.PredefinedType(SyntaxFactory.Token(SyntaxKind.DoubleKeyword));
-				case OpCodeOperand.ShortInlineR: return SyntaxFactory.PredefinedType(SyntaxFactory.Token(SyntaxKind.FloatKeyword));
+				case OpCodeOperand.InlineR:
+					return SyntaxFactory.PredefinedType(SyntaxFactory.Token(SyntaxKind.DoubleKeyword));
+				case OpCodeOperand.ShortInlineR:
+					return SyntaxFactory.PredefinedType(SyntaxFactory.Token(SyntaxKind.FloatKeyword));
 			}
 
 			return null;
